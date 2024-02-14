a45da0ebeff0b28d132bd11bd9101052eb46a2b9
Ari4nn4 <143848534+Ari4nn4@users.noreply.github.com>
2024-02-14 14:03:42 -0800

    Nosotras hemos terminado con el proyecto!

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,4 +1,19 @@
-// Your code here.
+// Yourl code here.
+loop(4,z=>z>0,z=>-1,console.log );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
--- 4-dominant-writing-direction.js
+++ 4-dominant-writing-direction.js
@@ -1,7 +1,9 @@
 require('./includes/scripts.js') // include the scripts used by the chapter
 
 function dominantDirection(text) {
-    // Your code here.
+    let string='skz';
+    console.log(string.length);
+    console.log("skz".repeat(7))
   }
   
   console.log(dominantDirection("Hello!"));
